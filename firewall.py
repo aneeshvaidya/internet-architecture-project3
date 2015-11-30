993b910e7245faaf18f11516381acffa1e6207b1
Aneesh <aneesh.h.vaidya@gmail.com>
2015-11-30 12:31:57 -0800

    Init functions for rules and geoipdb.

--- firewall.py
+++ firewall.py
@@ -10,6 +10,10 @@ class Firewall:
     def __init__(self, config, iface_int, iface_ext):
         self.iface_int = iface_int
         self.iface_ext = iface_ext
+        self.geo_dict = {}
+        self.rules_dict = {'TCP' : [], 'DNS' : [], 'HTTP' : []}
+        self.init_geo('geoipdb.txt')
+        self.init_rules(config['rule'])
 
         # TODO: Load the firewall rules (from rule_filename) here.
         print 'I am supposed to load rules from %s, but I am feeling lazy.' % \
@@ -17,6 +21,29 @@ class Firewall:
 
         # TODO: Load the GeoIP DB ('geoipdb.txt') as well.
         # TODO: Also do some initialization if needed.
+
+    def init_geo(self, f):
+        geoipdb = open(f, 'r')
+        line = geoipdb.readline()
+        while line:
+            line = line.split()
+            if line:
+                country_code = geo_line[2].upper()
+                if country_code in self.geo_dict.keys():
+                    self.geo_dict[country_code].append([line[0], line[1]])
+                else:
+                    self.geo_dict[country_code] = [[line[0], line[1]]]
+            line = geoipdb.readline()
+
+    def init_rules(self, f):
+        rules = open(f, 'r')
+        line = rules.readline()
+        while line:
+            line = line.lower().split()
+            if line and (line[0] in ['deny', 'log']):
+                self.rules_dict[line[1]].append(line)
+            line = rules.readline()
+
 
     # @pkt_dir: either PKT_DIR_INCOMING or PKT_DIR_OUTGOING
     # @pkt: the actual data of the IPv4 packet (including IP header)
